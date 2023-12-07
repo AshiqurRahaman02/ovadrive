8d7038c7137c4089e4f2e1a76b10a6a26a4b5b0a
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-12-07 18:51:33 +0530

    code pushing for future

---
+++ react-mic.d.ts
@@ -0,0 +1,3 @@
+declare module "react-mic" {
+	// export default ReactMic;
+}
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TranscribeAudio from './TranscribeAudio';
+
+function App() {
+  return (
+    <div className="App">
+      <TranscribeAudio/>
+    </div>
+  );
+}
+
+export default App;
